ad980f74de7605a781b99e8d737fe1a55ff5892d
VolodymyrTrykoz <mr.mastachok@gmail.com>
2017-11-29 16:52:14 +0200

    added notify

---
+++ Notify/js/js.js
@@ -0,0 +1,38 @@
+ $(document).ready(function(){
+	 $('.pict_slider').slick({
+	  infinite: true,
+	  slidesToShow: 19,
+  	  slidesToScroll: 10,
+  	  responsive: [
+                           
+                 {
+                   breakpoint: 1008,
+                   settings: {
+                   slidesToShow: 10,
+                   slidesToScroll: 5
+                             }
+                  }           
+
+                   ]
+	});//end slick 
+	$(function (){
+		$('.menuToggle').on('click', function(){
+
+			$('.menu').slideToggle(300, function(){
+
+				if($(this).css('display') === 'none') {
+						$(this).removeAttr('style');
+				}; // end this
+			}); // end slideToggle
+		}); // end menuToggle
+	}); // end function
+
+	$('.green').hover(function(){
+		$(this).css('opacity','0.5')
+	}, function (){
+		$(this).css('opacity','0')
+	}); // end hover
+
+});//end ready
+
+
